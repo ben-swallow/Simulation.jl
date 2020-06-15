590a73fd38c18d914b1f42cf228d2a39d77d1738
Sean Lovett <sean.lovett@invenialabs.co.uk>
2020-06-15 18:51:53 +0100

    Make grid a multi-D AxisArray

--- src/Epidemiology/EpiLandscape.jl
+++ src/Epidemiology/EpiLandscape.jl
@@ -7,13 +7,17 @@ Disease class abundances housed in the landscape. These are represented in both 
 mutable struct EpiLandscape
   matrix::Matrix{Int64}
   matrix_v::Matrix{Int64}
-  grid::Array{Int64, 3}
+  grid::AxisArray{Int64}
   seed::Vector{MersenneTwister}
-  function EpiLandscape(human_abun::Matrix{Int64}, virus_abun::Matrix{Int64}, d1::Tuple)
-    return new(human_abun, virus_abun, reshape(human_abun, d1), [MersenneTwister(rand(UInt)) for _ in 1:Threads.nthreads()])
+  function EpiLandscape(human_abun::Matrix{Int64}, virus_abun::Matrix{Int64}, ax)
+    d = length.(ax)
+    grid = AxisArray(reshape(human_abun, d), ax)
+    return new(human_abun, virus_abun, grid, [MersenneTwister(rand(UInt)) for _ in 1:Threads.nthreads()])
   end
-  function EpiLandscape(human_abun::Matrix{Int64}, virus_abun::Matrix{Int64}, d1::Tuple, d2::Tuple, seed::Vector{MersenneTwister})
-    return new(human_abun, virus_abun, reshape(human_abun, d1), seed)
+  function EpiLandscape(human_abun::Matrix{Int64}, virus_abun::Matrix{Int64}, ax, d2::Tuple, seed::Vector{MersenneTwister})
+    d = length.(ax)
+    grid = AxisArray(reshape(human_abun, d), ax)
+    return new(human_abun, virus_abun, grid, seed)
   end
 end
 
@@ -38,6 +42,6 @@ EpiList.
 function emptyepilandscape(epienv::GridEpiEnv, epilist::EpiList)
   mat_human = zeros(Int64, counttypes(epilist.human, true), countsubcommunities(epienv))
   mat_virus = zeros(Int64, counttypes(epilist.virus, true), countsubcommunities(epienv))
-  dimension = (counttypes(epilist.human, true), _getdimension(epienv.habitat)...)
-  return EpiLandscape(mat_human, mat_virus, dimension)
+  ax = AxisArrays.axes(epienv.initial_population)
+  return EpiLandscape(mat_human, mat_virus, ax)
 end
