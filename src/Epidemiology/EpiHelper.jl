f30f03b5a021135f9857bab7c237a6ca5c8d1f1a
Sean Lovett <sean.lovett@invenialabs.co.uk>
2020-06-19 18:00:23 +0100

    Fix to make grid_id use the right axes

--- src/Epidemiology/EpiHelper.jl
+++ src/Epidemiology/EpiHelper.jl
@@ -93,7 +93,7 @@ function simulate_record!(
 
   # - initialise and save the first timestep abuns/storage to HDF5
   # construct axes for abuns/storage matrix
-  grid_id = map(Iterators.product(axisvalues(epi.epienv.initial_population)...)) do (x,y)
+  grid_id = map(Iterators.product(axisvalues(epi.epienv.initial_population)[end-1:end]...)) do (x,y)
       return string.(x, "-", y)
   end
   # TODO: confirm converting `grid_id` from matrix to vector in the way below gives the
